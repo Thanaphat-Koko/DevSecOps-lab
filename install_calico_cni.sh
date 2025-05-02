d6c4fa86f1730d7352584b85804a7930198f77df
Thanaphat-Koko <thanaphat.peth@gmail.com>
2025-05-02 09:47:50 +0700

    update calico for HA k8s

--- install_calico_cni.sh
+++ install_calico_cni.sh
@@ -2,10 +2,12 @@
 set -e
 
 # 1. Apply Calico CNI
-kubectl apply -f https://raw.githubusercontent.com/projectcalico/calico/v3.27.2/manifests/calico.yaml
+kubectl create -f https://raw.githubusercontent.com/projectcalico/calico/v3.29.3/manifests/tigera-operator.yaml
+
+kubectl create -f https://raw.githubusercontent.com/projectcalico/calico/v3.29.3/manifests/custom-resources.yaml
 
 # 2. รอ Calico pod ขึ้นครบ
-echo "⏳ กำลังรอ Calico Pod ขึ้น..."
-kubectl rollout status daemonset/calico-node -n kube-system
+# echo "⏳ กำลังรอ Calico Pod ขึ้น..."
+# watch kubectl get pods -n calico-system
 
 echo "✅ Calico ติดตั้งเสร็จเรียบร้อย!"
