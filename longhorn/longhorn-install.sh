ebfdd964ebc619b588e95e2cd3e579696a8c257d
Thanaphat-Koko <thanaphat.peth@gmail.com>
2025-05-13 16:14:58 +0700

    create script for install longhorn storage class

---
+++ longhorn/longhorn-install.sh
@@ -0,0 +1,12 @@
+# Ref : https://longhorn.io/docs/1.8.1/deploy/install/install-with-helm/
+
+#!/bin/bash
+set -e
+
+helm repo add longhorn https://charts.longhorn.io
+
+helm repo update
+
+helm install longhorn longhorn/longhorn --namespace longhorn-system --create-namespace --version 1.8.1
+
+kubectl -n longhorn-system get pod
